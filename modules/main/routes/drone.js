792522e0cf6c83e08ff69cfdda9e58d6ecfe1774
Mardi Vester Tamma <mardi_tamma@yahoo.com>
2018-06-13 18:10:31 +0800

    add initial object for drone endpoint

---
+++ modules/main/routes/drone.js
@@ -0,0 +1,13 @@
+'use strcit';
+
+module.exports = function () {
+    // drone endpoint pair
+    this.endpoint = {
+        'PUT': {
+            'url': '',
+            'emiiter': '',
+        },
+    };
+    this.routeExecute = function (method, url) {
+    }
+};
---
+++ modules/main/routes/index.js
@@ -0,0 +1,7 @@
+const drone = require('./drone');
+
+module.exports = function () {
+    this.invokeRoute = function (method, url) {
+        
+    };
+};
